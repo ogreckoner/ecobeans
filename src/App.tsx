7274bef65b19aca8d2935fd26f20bdb6ed7ef45d
Carlos Febres <carlosfebres@dorg.tech>
2023-05-26 10:55:38 -0400

    hide swap feature

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   const routes = (
     <Routes>
       <Route path="/" element={<Home provider={provider} />} />
-      <Route path="/swap" element={<Swap provider={provider} />} />
+      {/*<Route path="/swap" element={<Swap provider={provider} />} />*/}
       <Route path="/about" element={<About />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
